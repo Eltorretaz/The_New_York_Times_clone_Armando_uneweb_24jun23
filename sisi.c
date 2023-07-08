f7916c2cff283cd61d7b5a2703b0b1c9ffa534ae
Eltorretaz <eltorretaz470@gmail.com>
2023-07-08 17:14:40 -0400

    DIa

---
+++ sisi.c
@@ -0,0 +1,17 @@
+main() {
+int n;
+int x = n-1;
+int ciclo;
+ciclo = n;
+
+  for(int i=0;i>n;i++){  
+    
+    if (x>0){
+      x--;
+    printf(" ");
+    }else if(x == 0) {
+      printf("#"); 
+    }
+    printf("\n");
+  }
+}
